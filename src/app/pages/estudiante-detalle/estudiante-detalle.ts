b69fa7d9bf81cd5f080d0f2f6fd8ebcecbb7ef6a
Jose Antonio Lazo <ja994285@gmail.com>
2025-11-20 20:49:55 -0500

    agregar cambios al ts

--- src/app/pages/estudiante-detalle/estudiante-detalle.ts
+++ src/app/pages/estudiante-detalle/estudiante-detalle.ts
@@ -1,11 +1,8 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-estudiante-detalle',
-  imports: [],
-  templateUrl: './estudiante-detalle.html',
-  styleUrl: './estudiante-detalle.css',
-})
-export class EstudianteDetalle {
-
-}
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { EstudianteService } from '../../services/estudiante.service';
+import { CursoService } from '../../services/curso.service';
+import { Estudiante } from '../../models/estudiante';
+import { Curso } from '../../models/curso';
+import { LoadingComponent } from '../../components/loading/loading.component';
